2bcf2d6a10dcb7bcd26c1aad443a83e656fd1562
Juan Amaya G <47118161+GaviriaAmaya@users.noreply.github.com>
2020-05-21 12:13:48 -0500

    [Fix] Delete conflicts with branches

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,10 +1,4 @@
 #Execute pkill on a process
 exec { 'killmenow':
-<<<<<<< HEAD
   command => '/usr/bin/pkill killmenow'
 }
-=======
-  command  => 'pkill killmenow',
-  provider => shell,
-}
->>>>>>> 1ffb65e190406f7a79f4e5de6b8756e688be1bdc
